6f9d381da0439dd4342d10b31bdf77f74bc24ead
unknown <sivak@SIVAK-IN.oradev.oraclecorp.com>
2018-12-05 16:56:54 +0530

    Add output and variables

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 resource "aws_instance" "demo" {
 	ami = "ami-84e3b2eb"
 	instance_type = "t2.micro"
-
+	vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 	user_data = <<-EOF
-#!/bin/bash
-echo "Hello World!" > index.html
-nohup busybox httpd -f -p 8080 &
-EOF
+					#!/bin/bash
+					echo "Hello World!" > index.html
+					nohup busybox httpd -f -p "${var.server_port}" &
+					EOF
 
 	tags {
 		Name = "demo-instance-1"
@@ -21,8 +21,8 @@ resource "aws_security_group" "instance" {
 	name = "demo-instance-1"
 
 	ingress {
-		from_port = 8080
-		to_port = 8080
+		from_port = "${var.server_port}"
+		to_port = "${var.server_port}"
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "server_port" {
+	description = "Port to connect to the server"
+	default = "8080"
+}
